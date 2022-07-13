df13dc42d8133f4ebf5e1ba8569376f669bdda97
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-07-13 08:55:07 +0300

    Done

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,8 +6,12 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
-    </div>
+      <div id="project-list">{/* render ProjectItem components here */}
+      {projects.map(prop => 
+      <ProjectItem key={prop.id} name={prop.name} about={prop.about} technologies={prop.technologies} id={prop.id} />)}
+      </div>
+      </div>
+    
   );
 }
 
